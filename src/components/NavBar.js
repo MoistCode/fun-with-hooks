d7c47bc374ceff308c914216651a25df1675e265
Tommy Long Pham <tpham@smugmug.com>
2020-01-16 18:28:05 -0800

    different way to consume ctx

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,19 +5,25 @@ export default class NavBar extends Component {
   static contextType = ThemeContext;
 
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    const { ui, syntax } = theme;
-
     return (
-      <nav style={{ background: ui, color: syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+            const { isLightTheme, light, dark } = context;
+            const theme = isLightTheme ? light : dark;
+            const { ui, syntax } = theme;
+            
+            return (
+              <nav style={{ background: ui, color: syntax }}>
+                <h1>Context App</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            )
+        }}
+      </ThemeContext.Consumer>
     )
   }
 }
